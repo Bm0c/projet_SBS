c420f9ff2553682623277631c4142052355c59d8
Bmoc <alexis-92400@hotmail.fr>
2012-12-11 21:24:23 +0100

    Ajout du divers.cs ...

--- Projet/Sunday Bloody Sunday/Sunday Bloody Sunday/Sunday Bloody Sunday/Divers.cs
+++ Projet/Sunday Bloody Sunday/Sunday Bloody Sunday/Sunday Bloody Sunday/Divers.cs
@@ -4,17 +4,15 @@ using Microsoft.Xna.Framework;
 namespace Sunday_Bloody_Sunday
 {
     static class Divers
-    {   
+    {
         // Récupération des paramètres relatif à la fenêtre de jeu
-        static private int widthScreen = 512;
-        static private int heightScreen = 512;
+        static private int widthScreen = 800;
+        static private int heightScreen = 480;
 
         static public int WidthScreen
         { get { return widthScreen; } }
 
         static public int HeightScreen
         { get { return heightScreen; } }
-
-        static public Vector2 HerosSquareOne = new Vector2(0, 0);
     }
 }
