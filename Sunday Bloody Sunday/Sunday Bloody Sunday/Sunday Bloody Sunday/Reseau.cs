10ee74546234c64199ffe3756773c509878b0168
Bm0c <alexis-92400@hotmail.fr>
2013-02-24 21:52:06 +0100

    Veritable fusion du samedi soir

---
+++ Sunday Bloody Sunday/Sunday Bloody Sunday/Sunday Bloody Sunday/Reseau.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Sunday_Bloody_Sunday
+{
+    class Reseau
+    {
+        public static List<Keys> Liste(string dl)
+        {
+            List<Keys> liste = new List<Keys>();
+            try
+            {
+                foreach (char a in dl)
+                {
+                    if (a == '1')
+                    {
+                        liste.Add(Keys.Z);
+                    }
+                    else if (a == '2')
+                    {
+                        liste.Add(Keys.S);
+                    }
+                    else if (a == '3')
+                    {
+                        liste.Add(Keys.Q);
+                    }
+                    else if (a == '4')
+                    {
+                        liste.Add(Keys.D);
+                    }
+                    else if (a == '5')
+                    {
+                        liste.Add(Keys.A);
+                    }
+                    else if (a == '6')
+                    {
+                        liste.Add(Keys.E);
+                    }
+                    else if (a == '7')
+                    {
+                        liste.Add(Keys.R);
+                    }
+                }
+            }
+            catch
+            {
+            }
+
+            return liste;
+
+        }
+
+        public static string Charger()
+        {
+            Reception();
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream flux = null;
+            try
+            {
+                flux = new FileStream("lecture", FileMode.Open, FileAccess.Read);
+                return (string)formatter.Deserialize(flux);
+            }
+            catch
+            {
+                //On retourne la valeur par défaut du type T.
+                return default(string);
+            }
+            finally
+            {
+                if (flux != null)
+                    flux.Close();
+            }
+
+
+        }
+
+        public static void Reception()
+        {
+            UdpClient serveur = new UdpClient(1337);
+
+
+            IPEndPoint client = null;
+            byte[] data = serveur.Receive(ref client);
+            string message = Encoding.Default.GetString(data);
+
+            StreamWriter ecriture = new StreamWriter("lecture");
+            ecriture.Write(message);
+            ecriture.Close();
+            serveur.Close();
+
+        }
+    }
+}
---
+++ Sunday Bloody Sunday/Sunday Bloody Sunday/Sunday Bloody Sunday/Sound.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Sunday_Bloody_Sunday
+{
+    class Sound
+    {
+        // FIELDS
+        SoundEffect tire = Ressources.mTire;
+        SoundEffect pika = Ressources.mPika;
+        SoundEffect pika2 = Ressources.mPika2;
+        SoundEffect raichu = Ressources.mRaichu;
+        SoundEffect raichu2 = Ressources.mRaichu2;
+        SoundEffect explosionEffect = Ressources.mExplosionEffect;
+        SoundEffect pop = Ressources.mPop;
+        SoundEffect bloodEffect = Ressources.mBloodEffect;
+        bool qui_crie = true;
+
+
+        // METHODS
+        public void PlayTire()
+        {
+            try
+            {
+                tire.Play();
+            }
+            catch
+            {
+            }
+        }
+
+        public void PlayPika()
+        {
+
+            Random random = new Random();
+            int randomNumber = random.Next(0, 4);
+            if (randomNumber < 2)
+            {
+                try
+                {
+                    pika.Play();
+
+                }
+                catch
+                {
+                }
+            }
+            else
+            {
+                try
+                {
+                    pika2.Play();
+
+                }
+                catch
+                {
+                }
+
+            }
+        }
+
+        public void PlayRaichu()
+        {
+
+            Random random = new Random();
+            int randomNumber = random.Next(0, 4);
+            if (randomNumber < 2)
+            {
+                try
+                {
+                    raichu.Play();
+                }
+                catch
+                {
+                }
+            }
+            else
+            {
+                try
+                {
+                    raichu2.Play();
+                }
+                catch
+                {
+                }
+            }
+        }
+
+
+
+
+        public void PlayExplosionEffect()
+        {
+            try
+            {
+                explosionEffect.Play();
+            }
+            catch
+            {
+            }
+        }
+
+
+        public void PlayBloodEffect()
+        {
+            try
+            {
+                bloodEffect.Play();
+            }
+            catch
+            {
+            }
+        }
+
+        public void PlayPop()
+        {
+            try
+            {
+                pop.Play();
+            }
+            catch
+            {
+            }
+        }
+    }
+}
